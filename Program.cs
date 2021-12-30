6dc3e39fe5b01e3d01d619e290675158eda654e8
Nykolas <nykolas577@gmail.com.com>
2021-12-30 11:18:05 -0300

    feat: exercise 1 to 7

---
+++ Program.cs
@@ -0,0 +1,208 @@
+﻿static void Excercise1A()
+{
+    //1)Crie um programa que imprima:
+    //Os números de 1 a 10 de forma crescente
+    
+    for (int counter = 1; counter < 10; counter++)
+    {
+        System.Console.WriteLine(counter);
+    }
+}
+
+static void Excercise1B()
+{
+    //1)Crie um programa que imprima:
+    //Os números de 1 a 10 de forma decrescente
+
+    for (int counter = 10; counter > 0; counter--)
+    {
+        System.Console.WriteLine(counter);
+    }
+
+}
+
+static void Excercise1C()
+{
+    //1)Crie um programa que imprima:
+    //Os números de 1 a 10 de forma crescente, mas apenas aqueles que forem par.
+
+    for (int counter = 2; counter < 11; counter+=2)
+    {
+        System.Console.WriteLine(counter);
+    }
+
+}
+
+static void Excercise2()
+{
+    //2)Imprimir a soma dos números inteiros de 1 a 100.
+    
+    int sum = 0;
+    for (int start = 1; start <= 100; start++)
+    {
+        sum += start;
+    }
+    System.Console.WriteLine($"A soma dos números de 1 a 100 é: {sum}");
+
+}
+
+static void Excercise3()
+{
+    //3)Imprimir todo os números ímpares menores de 200.
+
+    for (int counter = 1; counter < 200; counter += 2)
+    {
+        System.Console.WriteLine(counter);
+    }
+}
+
+static void Excercise4()
+{
+    //Calcular a média de idade de uma turma qualquer. 
+    //O algoritmo deve parar quando for digitada a idade igual a zero.
+
+    int counter = 0;
+
+    for (int entry = 0; true; entry++)
+    {
+        Console.WriteLine("Digite uma idade:");
+        var age = int.Parse(Console.ReadLine());
+        counter += age;
+
+        if (age == 0) 
+        {
+            Console.WriteLine($"A média da turma é: {counter / entry}");
+            break;
+        }
+    } 
+}
+
+static void Excercise5()
+{
+    // 5)Criar um algoritmo que peça o nome e a idade de 5 mulheres. Após informar estes dados, 
+    // o programa deve mostrar apenas porcentagem de mulheres que estão com idade entre 18 e 35.
+
+    var counter = 0;
+    int i = 0;
+    for (; i < 5; i++)
+    {
+        Console.WriteLine("Digite a idade de uma mulher");
+        var age = int.Parse(Console.ReadLine());
+
+        if (age > 17 && age < 36)
+        {
+            counter++;
+        }
+    }
+    
+    var percentage = (double)counter / i * 100;
+    Console.WriteLine($"A percentagem de mulheres que tem a idade entre 18 e 35 é {percentage}%");
+}
+
+static void Excercise6()
+{
+    //     Criar um algoritmo que simule uma urna eletrônica. Esta urna deve possuir dois candidatos
+    //     e possui três modos. O primeiro é o modo de CADASTRO, onde o sistema
+    //     pergunta o nome de cada um dos dois candidatos. Por medidas de segurança, para poder
+    //     cadastrar um candidato na urna o sistema deve solicitar ao usuário a senha para poder ter
+    //     acesso(a senha do sistema é Pa$$w0rd). O segundo modo é o modo de votos, onde o usuário 
+    //     informa 1 para votar no primeiro candidato e 2 para votar no segundo candidato. O terceiro modo,
+    //    é o modo de apuração de votos, onde o sistema verifica qual candidato tem mais votos. Caso o 
+    //     número de votos seja igual, o sistema deve imprimir a mensagem "SEGUNDO TURNO",
+    //    caso contrário deve imprimir o nome do candidato vencedor e o número de votos que ele obteve.
+
+    (string? name, int vote) firstCandidate = (null, 0);
+    (string? name, int vote) secondCandidate = (null, 0);
+
+    while (true)
+    {
+        Console.WriteLine("Selecione um dos modos abaixo:");
+        Console.WriteLine("1) Cadastro");
+        Console.WriteLine("2) Votos");
+        Console.WriteLine("3) Apuração de votos");     
+        var mode = int.Parse(Console.ReadLine());
+
+        if (mode == 1)
+        {
+            var password = "";
+            do
+            {
+                Console.WriteLine("Digita sua senha:");
+                password = Console.ReadLine();
+            } while (password != "Pa$$w0rd");
+
+            Console.WriteLine("Digite o nome do 1º candidato");
+            firstCandidate.name = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome do 2º candidato");
+            secondCandidate.name = Console.ReadLine();
+
+        }
+
+        else if (mode == 2)
+        {
+            Console.WriteLine($"Digite 1 para o candidato {firstCandidate.name}");
+            Console.WriteLine($"Digite 2 para o candidato {secondCandidate.name}");
+            var candidateToVote = int.Parse(Console.ReadLine());
+
+            if (candidateToVote == 1) firstCandidate.vote++;
+            
+            else if (candidateToVote == 2) secondCandidate.vote++;
+        }
+
+        else if (true)
+        {
+            if (firstCandidate.vote > secondCandidate.vote)
+            {
+                Console.WriteLine($"O candidato {firstCandidate.name} ganhou de {secondCandidate.name} com o total de {firstCandidate.vote} votos");
+            }
+
+            else if (firstCandidate.vote < secondCandidate.vote)
+            {
+                Console.WriteLine($"O candidato {secondCandidate.name} ganhou de {firstCandidate.name} com o total de {secondCandidate.vote} votos");
+            }
+
+            else
+            {
+                Console.WriteLine("Segundo turno");
+                continue;
+            }
+
+            break;
+        }
+    }
+}
+
+static void Excercise7()
+{
+    
+}
+
+static void Excercise9()
+{
+    //9)Fazer um algoritmo para ler 03 números reais do teclado e verificar se o 
+    //primeiro é maior que a soma dos outros dois.
+    
+    // if (firstNumber > (secondNumber + thirdNumber))
+    // {
+    //     Console.WriteLine($"{firstNumber} é maior que a soma de {secondNumber} e {thirdNumber}");
+    // }
+    // else
+    // {
+    //     Console.WriteLine($"{firstNumber} é menor que a soma de {secondNumber} e {thirdNumber}");
+    // }
+    
+    Console.WriteLine("Digite o 1º número");
+    var firstNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite 2º número");
+    var secondNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite 3º número");
+    var thirdNumber = int.Parse(Console.ReadLine());
+    
+    var isBigger = firstNumber > (secondNumber + thirdNumber)
+        ? "maior"
+        : "menor";
+    Console.WriteLine($"{firstNumber} é {isBigger} que a soma de {secondNumber} com {thirdNumber}");
+}
+
+Excercise6();
---
+++ README.md
@@ -0,0 +1,229 @@
+# Projeto Entra21
+
+### Escrevendo e lendo do console
+    // Escrever
+    Console.WriteLine("Qual é a sua idade?");
+    
+    // Ler e salvar o resultado
+    var result = Console.ReadLine();
+
+    // Juntar textos
+    string text = $"Olá, {result}";
+
+### Declarando variáveis e constantes
+    var text = "meu texto"
+    string text = "meu texto"
+    const string text = "meu texto"
+
+### Tipos de variáveis
+    // Tipo numérico/int
+    var number = 1
+
+    // Tipo texto/string
+    var text = "meu texto"
+    
+    // Tipo booleano/bool
+    var boolean = true
+
+    // Tipo numérico com ponto flutuante/double
+    var price = 4.099
+
+    // Tipo Data/DateTime
+    var currentDate = DateTime.Now;
+
+    // Tipo Nullable Double
+    double? x = null;
+
+    // Lista/Array de 1 posição
+    var names = new string[1];
+    names[0] = "Lucas";
+    // names terá o valor Lucas na posição de número 0
+
+    // Tupla/Tuple
+    (string name, int votes) firstCandidate = ("", 0)
+
+    //tuple/array
+    var candidates = new (string name, int votes)[3];
+
+### Parsers / Transformadores
+    // Transformar um texto/string em um número/int
+    var userAge = Int.Parse(result);
+
+    // Transformar um texto/string em um número com ponto flutuante/double
+    double price = Double.Parse(result);
+
+    // Formatar número/double para até duas casas decimais após a vírgula
+    25.658499.ToString("0.00")
+    // 25.66
+
+### Operadores de comparação
+#### (toda comparação retorna um booleano/bool)
+    // É igual?
+    1 == 1;
+    // vai retornar true
+
+    // É diferente?
+    1 != 1;
+    // vai retornar false
+    
+    // É diferente?
+    !(1 == 1);
+    // vai retornar false
+
+    // Booleano da esquerda é true e o da direita também?
+    true && true && false;
+    // vai retornar false
+
+    // Booleano da esquerda ou o da direita é true?
+    false || false || true;
+    // vai retornar true
+
+    // Operador ternário
+    var myBool = true;
+    var message = myBool ? "Solteiro" : "Não solteiro";
+    // retornará "Solteiro" quando myBool for True ou "Não solteiro quando for False
+
+### Operadores de comparação para números/matemática
+    // É maior?
+    1 > 1;
+    // vai retornar false
+
+    // É menor?
+    1 < 1;
+    // vai retornar false
+    
+    // É maior OU igual?
+    1 >= 1;
+    // vai retornar true
+    
+    // É menor OU igual?
+    1 <= 1;
+    // vai retornar true
+
+### Operadores de matemática
+    // Somar
+    1 + 1;
+
+    // Subtrair
+    1 - 1;
+    
+    // Dividir
+    2 / 1;
+    
+    // Multiplicar
+    1 * 2;
+
+    // Resto
+    3 % 2;
+    // vai retornar 1
+    
+    // Incrementando uma variável (2 vezes)
+    var counter = 0;
+    var counter++;
+    var counter += 1;
+    // counter terá o valor 2
+
+    // Decrementando uma variável
+    var counter = 10;
+    var counter--;
+    // counter terá o valor 9
+
+### Blocos condicionais
+    // Escrevendo olá SOMENTE quando 1 > 0
+    if (1 > 0)
+    {
+       Console.WriteLine("Olá")
+    }
+    // Caso 1 NÃO for maior que 0, e 2 > 1, escreva Boa tarde!
+    else if (2 > 1)
+    {
+       Console.WriteLine("Boa tarde!")
+    }
+    // Caso NENHUMA das condições anteriores forem verdadeiras, escreva Xau!
+    else
+    {
+       Console.WriteLine("Xau!")
+    }
+
+### Laços de repetição
+    // while
+    while (true)
+    {
+        Console.WriteLine("Olá!");
+    }
+    // Dirá olá infinitamente
+
+    // forçando para que um laço de repetição pare
+    while (true)
+    {
+        break;
+    }
+
+    // continuando o looping pulando o restante do código
+    while (true)
+    {
+        continue;
+
+        ....
+    }
+
+    // for (imprimindo de 0 a 9)
+    for (int counter = 0; counter < 10; counter++)
+    {
+        System.Console.WriteLine(counter);
+    }
+
+    // foreach (imprimindo todos os elementos do array "numbers")
+    foreach (var item in numbers)
+    {
+        Console.WriteLine(item);
+    }
+
+### Capturando exceções
+    Console.WriteLine("Forneça um número");
+    string firstInput = Console.ReadLine();
+    
+    try
+    {
+        var firstNumber = Int32.Parse(firstInput);
+    }
+    catch (System.Exception)
+    {
+        System.Console.WriteLine("O números está em formato inválido");
+    }
+
+### Métodos
+    // Retornando de um método:
+    return;
+
+### Comandos do editor
+Selecionando o texto:
+Segurar a tecla shift e utilizar as setas
+
+Comando para comentar:
+CTRL + K + C
+
+Comando para descomentar:
+CTRL + K + U
+
+Comando para desfazer:
+CTRL + Z
+
+Comando para refazer:
+CTRL + Y
+
+Comando para abrir o terminal:
+CTRL + '
+
+### Depurando a aplicação
+
+1. Clicar na baratinha e depois no botao de engrenagem;
+2. Selecionar o texto '.Net Core'
+3. Alterar a opção "console" para "integratedTerminal"
+4. Adicionar quantos breakpoints quiser (clicar na esquerda do número da linha)
+5. Clicar na baratinha e no botão Start (verdinho)
+6. Selecionar o Terminal (Sair na tab "Debug Console")
+7. F10 vai para a próxima linha
+8. F5 para o próximo breakpoint
+9. Shift + F5 para "matar" a aplicação
+10. CTRL + Shift + F5 para reiniciar o debug
