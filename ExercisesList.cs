358b503a7df52ae46cbde10c04b013caa3cbd55a
viniciusoliveira-boxti <vinicius.oliveira@boxti.com.br>
2021-12-31 11:55:41 -0300

    feat: repeat exercises

--- ExercisesList.cs
+++ ExercisesList.cs
@@ -31,21 +31,28 @@ namespace ExercisesLists
             {
                 Console.WriteLine($"Digite {item.Key} para executar o exercício {item.Value.Method.Name}");
             }
-            var chosenExercise = Console.ReadLine();
+            var chosenExercise = Console.ReadLine().ToUpper();
 
             try
             {
-                selectedExerciseList[chosenExercise]();
-                
-                Console.WriteLine("\nDeseja executar outro exercício? Tecle (S) para sim, tecle (R) para repetir exercício ou (N) para encerrar");
-                var afterExercise = Console.ReadLine().ToLower();
-                
-                if (afterExercise == "s") goto start;
-                else if (afterExercise == "r") selectedExerciseList[chosenExercise]();
+                while (true)
+                {
+                    selectedExerciseList[chosenExercise]();
+                    
+                    Console.WriteLine("\nDeseja executar outro exercício? Tecle (S) para sim, tecle (R) para repetir exercício ou (N) para encerrar");
+                    var afterExercise = Console.ReadLine().ToLower();
+                    
+                    if (afterExercise == "s") goto start;
+                    else if (afterExercise == "r") continue;
+                    
+                    break;
+                }
             }
             catch (Exception ex)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine($"Ocorreu um erro ao executar o exercício. Exception: {ex.Message}");
+                Console.ForegroundColor = ConsoleColor.White;
             }
         }
     }
